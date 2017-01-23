9d94602cbdf41e2f6c41172822b209fb8e545f85
Michael Roque <mikeroque1@gmail.com>
2017-01-23 10:26:00 -0700

    fixed unique by adding ()

--- migrations/20170123093700_user.js
+++ migrations/20170123093700_user.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('user', function(table){
     table.increments()
     table.text('name').notNullable()
-    table.text('email').notNullable().unique
+    table.text('email').notNullable().unique()
     table.text('password').notNullable()
   })
 };
